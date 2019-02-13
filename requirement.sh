0e62e92e6be5012752d927e1fde0b6df9e422029
KoinWorks <benedictoharyono@Bens-MacBook-Pro.local>
2019-02-14 00:35:10 +0700

    requirement.sh: Add new requirement dotmap

--- requirement.sh
+++ requirement.sh
@@ -5,5 +5,6 @@ pip3 install bunch==1.0.1
 pip3 install tqdm==4.28.1
 pip3 install opencv-python==3.4.3.18
 pip3 install numpy==1.13.1
+pip3 install dotmap
 pip3 install git+https://github.com/epigramai/tfserving-python-predict-client.git
 #etc.
