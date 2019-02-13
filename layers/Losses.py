c9b3b447ef9364855249b422c91bce817344cd9b
KoinWorks <benedictoharyono@Bens-MacBook-Pro.local>
2019-02-14 00:34:47 +0700

    Losses.py: Add several loss function; contrastive loss and distance based logistic loss

--- layers/Losses.py
+++ layers/Losses.py
@@ -1,14 +1,23 @@
 import tensorflow as tf
 
 
-def contrastive_loss(input_1, input_2, y, margin, eps):
-    with tf.name_scope('contrastive_loss'):
-        distance = tf.sqrt(eps + tf.reduce_sum(
-            tf.pow(input_1 - input_2, 2), 1, keepdims=True))
+def contrastive_loss(input_1, input_2, label, margin, eps):
+    with tf.name_scope('contrastive_loss') as scope:
+        y = tf.to_float(label)
+        distance = euclidean_distance(input_1, input_2, eps)
         similarity = y * tf.square(distance)
-        disimilarity = (1 - y) * tf.square(
-            tf.maximum(margin - distance, 0))
-        loss = tf.reduce_mean(disimilarity + similarity) / 2
+        disimilarity = (1.0 - y) * tf.square(
+            tf.maximum((margin - distance), 0.0))
+        loss = tf.reduce_mean(disimilarity + similarity) / 2.0
+    return loss
+
+
+def DBL_loss(input_1, input_2, label, margin, eps):
+    with tf.name_scope('distance_based_logistic') as scope:
+        y = tf.expand_dims(tf.to_float(label), axis=1)
+        distance = euclidean_distance(input_1, input_2, eps)
+        p = (1.0 + tf.exp(-margin)) / (1 + tf.exp(distance - margin))
+        loss = tf.losses.log_loss(y, p)
     return loss
 
 
@@ -24,6 +33,10 @@ def triplet_loss(acnchor, positive, negative):
     return loss
 
 
-def eculidean_distance(input_1, input_2):
-    return tf.sqrt(
-        tf.reduce_sum(tf.pow(input_1, input_2, 2), 1, keepdims=True))
+def euclidean_distance(input_1, input_2, eps=1e-6):
+    with tf.name_scope('euclidean_distance') as scope:
+        distance = tf.sqrt(
+            eps + tf.reduce_sum(
+                tf.square(tf.subtract(
+                    input_1, input_2)), axis=1, keepdims=True))
+    return distance
