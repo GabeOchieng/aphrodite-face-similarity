9452a38710d30c1878ca6b8e9c493c945cafb2ce
KoinWorks <benedictoharyono@Bens-MacBook-Pro.local>
2019-02-14 00:38:17 +0700

    Main.py: Change convolution model name regarding the declared class

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
-from dataloader.DataLoader import DataLoader
-from model.VGG16 import VGG16
+from dataloader.SiameseDataLoader import SiameseDataLoader
+from model.ConvNet import ConvNet
 from trainer.Trainer import Trainer
 from utils.Parser import process_config
 from utils.Logger import Logger
@@ -12,21 +12,21 @@ def main():
     args = get_args()
     m_config = process_config(args.config)
 
-    config = tf.ConfigProto(log_device_placement=True)
+    config = tf.ConfigProto(log_device_placement=False)
     config.gpu_options.allow_growth = True
 
     with tf.Session(config=config) as sess:
         # create_dirs([config.summary_dir, config.checkpoint_dir])
-        data = DataLoader(config=m_config)
-        model = VGG16(data_loader=data, config=m_config)
+        data_loader = SiameseDataLoader(config=m_config)
+        model = ConvNet(data_loader=data_loader, config=m_config)
         logger = Logger(sess=sess, config=m_config)
 
         trainer = Trainer(
             sess=sess,
             model=model,
-            data=data,
             config=m_config,
-            logger=logger)
+            logger=logger,
+            data_loader=data_loader)
 
         trainer.train()
 
