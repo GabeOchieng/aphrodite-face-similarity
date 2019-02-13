d7d3e2a576898af66ff23c3b4990798c3d5432fd
KoinWorks <benedictoharyono@Bens-MacBook-Pro.local>
2019-02-14 00:33:59 +0700

    Parser.py: Change bunch to DotMap

--- utils/Parser.py
+++ utils/Parser.py
@@ -1,19 +1,19 @@
 import os
 import json
-from bunch import Bunch
+from dotmap import DotMap
 
 
 def get_config_from_json(json_file):
     with open(json_file, 'r') as config_file:
         config_dict = json.load(config_file)
-    config = Bunch(config_dict)
+    config = DotMap(config_dict)
     return config, config_dict
 
 
 def process_config(json_file):
     config, _ = get_config_from_json(json_file)
     config.summary_dir = os.path.join(
-        "experiments", config.project_name, "summary/")
+        "experiments", config.exp.project_name, "summary/")
     config.checkpoint_dir = os.path.join(
-        "experiments", config.project_name, "checkpoint/")
+        "experiments", config.exp.project_name, "checkpoint/")
     return config
