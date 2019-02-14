f67300cfdc804b119ea73f1dbac3a04ec8788817
KoinWorks <benedictoharyono@Bens-MacBook-Pro.local>
2019-02-14 16:45:07 +0700

    Convolutions.py: Change variable_scope in maxpool function to name_scope

--- layers/Convolutions.py
+++ layers/Convolutions.py
@@ -3,10 +3,11 @@ import tensorflow as tf
 
 def conv_layer(inputs, filters, k_size,
                stride, padding, scope_name, 
-               reuse=False, active=False):
+               reuse=False, active=True):
 
     with tf.variable_scope(scope_name, reuse=reuse) as scope:
         in_channels = inputs.shape[-1]
+
         kernel = tf.get_variable(
             'kernel',
             [k_size, k_size, in_channels, filters],
@@ -23,27 +24,29 @@ def conv_layer(inputs, filters, k_size,
             biases)
 
         if active:
-            f_conv = tf.nn.tanh(conv)
-        else:
             f_conv = tf.nn.relu(conv)
 
     return f_conv
 
 
-def maxpool(inputs, k_size,
-            stride, padding='VALID', scope_name='maxpool'):
+def maxpool(inputs, k_size, stride, 
+            padding='VALID', scope_name='maxpool'):
 
-    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+    with tf.name_scope(scope_name) as scope:
         pool = tf.nn.max_pool(
-            inputs, ksize=[1, k_size, k_size, 1],
-            strides=[1, stride, stride, 1], padding=padding)
+            inputs, 
+            ksize=[1, k_size, k_size, 1],
+            strides=[1, stride, stride, 1], 
+            padding=padding)
+
     return pool
 
 
 def fully_connected(inputs, out_dim,
-                    scope_name, activation=tf.nn.sigmoid):
+                    scope_name, reuse,
+                    activation=tf.nn.sigmoid):
 
-    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+    with tf.variable_scope(scope_name, reuse=reuse) as scope:
         in_dim = inputs.shape[-1]
         w = tf.get_variable(
             'weights', [in_dim, out_dim],
@@ -51,8 +54,8 @@ def fully_connected(inputs, out_dim,
         b = tf.get_variable(
             'biases', [out_dim],
             initializer=tf.random_normal_initializer())
-        out = tf.nn.bias_add(tf.matmul(inputs, w), b)
+        out = tf.nn.bias_add(tf.matmul(inputs, w), b, name='linear')
 
         if activation:
-            out = activation(out)
+            out = activation(out, name='activation')
         return out
